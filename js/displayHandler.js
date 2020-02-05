120782fbe4050742d083a8192775211cb8c1c1f4
Daniel Arango <46094308+Zendrians@users.noreply.github.com>
2020-02-05 14:48:05 -0500

    add display handler logic

---
+++ js/displayHandler.js
@@ -0,0 +1,30 @@
+const cursosBtn = document.querySelector('#cursos-btn');
+const preguntasBtn = document.querySelector('#preguntas-btn');
+const cursosBox = document.querySelector('#cursos-box');
+const preguntasBox = document.querySelector('#preguntas-box');
+
+preguntasBtn.addEventListener('click', () => {
+    cursosBox.style.display = "none";
+    preguntasBox.style.display = "block";
+    preguntasBtn.classList.add('control-active');
+    cursosBtn.classList.remove('control-active');
+});
+
+cursosBtn.addEventListener('click', () => {
+    cursosBox.style.display = "block";
+    preguntasBox.style.display = "none";
+    cursosBtn.classList.add('control-active');
+    preguntasBtn.classList.remove('control-active');
+});
+
+window.addEventListener('resize', () => {
+    if (document.documentElement.clientWidth >= 1100) {
+        preguntasBox.style.display = "block";
+        cursosBox.style.display = "block";
+    } else  {
+        preguntasBox.style.display = "block";
+        cursosBox.style.display = "none";
+        preguntasBtn.classList.add('control-active');
+        cursosBtn.classList.remove('control-active');
+    }
+});
